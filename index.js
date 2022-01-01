b1bfb4821d24deb5d88d5f62f5076253b6d3ea68
GermanGambarte <germangambarte@gmail.com>
2022-01-01 18:03:20 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,81 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const uuid = require('uuid/v4')
+
+app.use(cors())
+app.use(express.json())
+
+let gifts = [
+  {
+    id: 1,
+    gift: 'Chocolate',
+    quantity: 3,
+    price: 10,
+    owner: 'Olga',
+  },
+  {
+    id: 2,
+    gift: 'Remera',
+    quantity: 2,
+    price: 20,
+    owner: 'Raul',
+  },
+  {
+    id: 3,
+    gift: 'Vitel Toné',
+    quantity: 1,
+    price: 50,
+    owner: 'Teresa',
+  },
+  {
+    id: 4,
+    gift: 'Caramelos',
+    quantity: 5,
+    price: 12,
+    owner: 'Horacio',
+  },
+]
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World</h1>')
+})
+
+app.get('/api/gifts', (request, response) => {
+  response.json(gifts)
+})
+
+app.get('/api/gifts/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const gift = gifts.find((gift) => gift.id === id)
+  response.send(gift)
+})
+
+app.delete('/api/gifts/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const gift = gifts.filter((gift) => gift.id !== id)
+  response.send(gift)
+})
+
+app.post('/api/gifts', (request, response) => {
+  const gift = request.body
+  if (!gift || !gift.gift) {
+    return response.status(400).json({
+      error: 'gift.gift is missing',
+    })
+  }
+  const newGift = {
+    id: uuid(),
+    gift: gift.gift,
+    quantity: gift.quantity,
+    price: gift.price,
+    owner: gift.owner,
+  }
+  gifts = [...gifts, newGift]
+  response.json(newGift)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`)
+})
