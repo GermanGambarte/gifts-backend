54b5fbc99406cc4537cd050b014af2e8db06e61e
GermanGambarte <germangambarte@gmail.com>
2022-01-18 17:10:29 -0300

    Solving mongoose error

--- mongo.js
+++ mongo.js
@@ -3,8 +3,11 @@ const mongoose = require('mongoose')
 const connectionString = process.env.MONGO_DB_URI
 
 mongoose
-  .connect(connectionString)
+  .connect(connectionString, { useNewUrlParser: true })
   .then(() => console.log('DataBase connected'))
   .catch((err) => console.error(err))
+mongoose.connection
+  .once('open', () => console.log('Conection has been made!'))
+  .on('error', (error) => console.log('Error is: ', error))
 
-process.on('uncaughtException', () => mongoose.connection.disconnect())
+// process.on('uncaughtException', () => mongoose.connection.disconnect())
