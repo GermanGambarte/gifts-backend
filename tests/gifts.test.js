b2c08b10cffe3878414dde12c0eff129532a6a78
GermanGambarte <germangambarte@gmail.com>
2022-01-27 13:58:56 -0300

    cleaned up

--- tests/gifts.test.js
+++ tests/gifts.test.js
@@ -1,22 +1,22 @@
-const mongoose = require('mongoose')
-const supertest = require('supertest')
-const { app, server } = require('../index')
+// const mongoose = require('mongoose')
+// const supertest = require('supertest')
+// const { app, server } = require('../index')
 
-const api = supertest(app)
+// const api = supertest(app)
 // test('notes are returned as json', async () => {
 //   await api
 //     .get('/api/gifts')
 //     .expect(200)
 //     .expect('Content-Type', /application\/json/)
 // })
-test('there are two gifts', async () => {
-  const response = await api.get('/api/gifts')
-  expect(response.body).toHaveLength(1)
-})
-afterAll(() => {
-  mongoose.connection.close()
-  server.close()
-})
+// test('there are two gifts', async () => {
+//   const response = await api.get('/api/gifts')
+//   expect(response.body).toHaveLength(1)
+// })
+// afterAll(() => {
+//   mongoose.connection.close()
+//   server.close()
+// })
 // const {palindrome}= require('../utils/for_testing')
 // test('palindrome of german', ()=> {
 //   const result = palindrome('german')
